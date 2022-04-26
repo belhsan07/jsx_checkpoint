893a79a8926da8986c5bb73526281a0780438b0b
belhsan07 <neromvfiv@gmail.com>
2022-04-26 14:14:21 +0100

    checkpoint_jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import myVideo from "./myVideo.mp4";
+import imageSrc from "./imageInSrc.jpg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red"> Belhsan Balti </h1>
+
+        <br />
+
+        <img src={imageSrc} alt=" imageInSrc" />
+
+        <br />
+
+        <img src="/imageInPublic.png" alt="imageInPublic" />
+      </div>
+
+      <video width="320" height="240" controls>
+        <source src={myVideo} type="video/mp4" />
+      </video>
     </div>
   );
 }
